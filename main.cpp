00587a8020e7b9c136402c59a729e4ba86103e39
Benjamin <BECOD1@ulaval.ca>
2024-10-05 03:21:39 -0400

    moved old code

--- main.cpp
+++ main.cpp
@@ -4,23 +4,9 @@
 #include <cmath>
 #include <date.h>
 #include <vulkanapp.cpp>
-#include <rendering.cpp>
 
 int main()
 {
-    //Setup logging
-    freopen( "log.txt" , "a", stdout);
-
-    auto now = std::chrono::system_clock::now();
-    std::string formatTime = date::format("%T --- %F", now);
-    std::cout << "\nLOG --- " << formatTime << '\n';
-
-    printf("\n");
-
-    const int WIDTH = 512, HEIGHT = 512;
-    //renders scene in a ppm file
-    renderScene(WIDTH, HEIGHT);
-
     VulkanApp app;
 
     try
